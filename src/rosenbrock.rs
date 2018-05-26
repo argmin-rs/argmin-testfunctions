1bc4121d9649f224b5a4e766d9a828881e2f977e
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-05-26 19:27:38 +0200

    assert that parameter vector has a length of two, speed improvements for rosenbrock functions

--- src/rosenbrock.rs
+++ src/rosenbrock.rs
@@ -12,20 +12,20 @@ use num::{Float, FromPrimitive};
 /// Rosenbrock test function
 ///
 /// Parameters are usually: `a = 1` and `b = 100`
-/// TODO: make this multidimensional
 pub fn rosenbrock<T: Float + FromPrimitive>(param: &[T], a: T, b: T) -> T {
+    assert!(param.len() == 2);
     let num2 = T::from_f64(2.0).unwrap();
     (a - param[0]).powf(num2) + b * (param[1] - param[0].powf(num2)).powf(num2)
 }
 
 /// Derivative of 2D Rosenbrock function
 pub fn rosenbrock_derivative<T: Float + FromPrimitive>(param: &[T], a: T, b: T) -> Vec<T> {
+    assert!(param.len() == 2);
     let num2 = T::from_f64(2.0).unwrap();
     let num3 = T::from_f64(3.0).unwrap();
     let num4 = T::from_f64(4.0).unwrap();
-    let x = param[0];
-    let y = param[1];
-    let mut out = vec![];
+    let (x, y) = (param[0], param[1]);
+    let mut out = Vec::with_capacity(2);
     out.push(-num2 * a + num4 * b * x.powf(num3) - num4 * b * x * y + num2 * x);
     out.push(num2 * b * (y - x.powf(num2)));
     out
@@ -33,12 +33,12 @@ pub fn rosenbrock_derivative<T: Float + FromPrimitive>(param: &[T], a: T, b: T) 
 
 /// Hessian of 2D Rosenbrock function
 pub fn rosenbrock_hessian<T: Float + FromPrimitive>(param: &[T], _a: T, b: T) -> Vec<T> {
+    assert!(param.len() == 2);
     let num2 = T::from_f64(2.0).unwrap();
     let num4 = T::from_f64(4.0).unwrap();
     let num12 = T::from_f64(12.0).unwrap();
-    let x = param[0];
-    let y = param[1];
-    let mut out = vec![];
+    let (x, y) = (param[0], param[1]);
+    let mut out = Vec::with_capacity(4);
     // d/dxdx
     out.push(num12 * b * x.powf(num2) - num4 * b * y + num2);
     // d/dxdy
@@ -71,5 +71,11 @@ mod tests {
         for elem in &res {
             assert!((elem - 0.0).abs() < std::f32::EPSILON);
         }
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_rosenbrock_not_2d() {
+        rosenbrock(&[1.0, 1.0, 1.0], 1.0, 100.0);
     }
 }

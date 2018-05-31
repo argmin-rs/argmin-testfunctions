d466f91e51e49e3fc2116b60816fa08a9195a5fa
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-05-31 10:46:36 +0200

    picheny test function

--- benches/testfunctions.rs
+++ benches/testfunctions.rs
@@ -39,6 +39,8 @@ mod tests {
 
     make_bench!(goldsteinprice(&vec![-4.0, 3.0]));
 
+    make_bench!(picheny(&vec![0.5, 0.9]));
+
     make_bench!(rastrigin(&vec![-43.0, 53.0, 3.4]));
     make_bench!(rastrigin_a(&vec![-43.0, 53.0, 3.4], 10.0));
 

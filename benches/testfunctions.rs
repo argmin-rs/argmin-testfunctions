84aa72dec45469b10dc71a00f88ffd180bb8ceed
Stefan Kroboth <stefan.kroboth@gmail.com>
2018-06-10 09:53:29 +0200

    schaffer n2 and n4 test functions

--- benches/testfunctions.rs
+++ benches/testfunctions.rs
@@ -74,6 +74,9 @@ mod tests {
     make_bench!(sphere(&vec![-43.0, 53.0]));
     make_bench!(sphere_derivative(&vec![-43.0, 53.0]));
 
+    make_bench!(schaffer_n2(&vec![-43.0, 53.0]));
+    make_bench!(schaffer_n4(&vec![-43.0, 53.0]));
+
     make_bench!(threehumpcamel(&vec![-43.0, 53.0]));
 
     make_bench!(zero(&vec![-43.0, 53.0]));
